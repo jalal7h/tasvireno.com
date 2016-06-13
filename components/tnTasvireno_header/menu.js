c0307eea14587f3367415a7abc49b34977be8c85
Meysam Taghipoor <taghipoor.meysam@gmail.com>
2016-06-13 20:23:07 +0430

    منو رسپانسیو

--- components/tnTasvireno_header/menu.js
+++ components/tnTasvireno_header/menu.js
@@ -9,11 +9,40 @@ $(document).ready(function(){
    	content3 = $("#field1");
  	content3.load("./?do_action=menu_ajax&do=field");
 	
-	$(".menu > ul > li").hover(function (e) {
-        
-            $(this).children("ul").stop(true, false).fadeToggle(0.0000001);
+	$('.menu > ul > li:has( > ul)').addClass('menu-dropdown-icon');
+
+
+    $('.menu > ul > li > ul:not(:has(ul))').addClass('normal-sub');
+
+    $(".menu > ul").before("<a href=\"#\" class=\"menu-mobile\"></a>");
+
+
+    $(".menu > ul > li").hover(function (e) {
+        if ($(window).width() > 943) {
+            $(this).children("ul").stop(true, false).fadeToggle(0.0001);
             e.preventDefault();
-       
+        }
+    });
+
+    $("#gift , #brand , #field ").click(function (e) {
+        if ($(window).width() <= 943) {
+            $(this).children("ul").fadeToggle(200);
+            e.preventDefault();
+        }
+    });
+
+    $(".menu-mobile").click(function (e) {
+        $(".menu > ul").slideToggle(0.0000003);
+        e.preventDefault();
+    });
+
+    $(window).resize(function(){
+       if ($('.menu').width() > 944) {
+       	
+    $(".menu > ul").slideDown(0.0000003);
+    $(".menu > ul > li > ul ").fadeOut(0.0000003);
+    
+	}
     });
 
 });
