d2ffd9b68a8ca73e93764e38ce11c8e34d2db343
Jalal Hoseini <jalal7h@gmail.com>
2016-06-14 09:31:30 +0430

    raf e moshkel e texty_msg dar ersal email.

--- components/texty-2/texty_msg.php
+++ components/texty-2/texty_msg.php
@@ -23,7 +23,7 @@ function texty_msg( $who , $template_name , $vars ){
 	switch ($who) {
 
 		case 'admin':
-			$to = tab__temp('email_address_webadmin');
+			$to = tab__temp('contact_email_address_0');
 			break;
 
 		case 'user':
