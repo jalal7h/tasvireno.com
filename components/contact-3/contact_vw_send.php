128a9af24b780b1cc997ebe1acef925fd81981ab
Meysam Taghipoor <taghipoor.meysam@gmail.com>
2016-06-19 12:11:14 +0430

    ارتباط با ما 3

--- components/contact-3/contact_vw_send.php
+++ components/contact-3/contact_vw_send.php
@@ -2,7 +2,11 @@
 
 function contact_vw_send(){
 
-	if(! $_REQUEST['to'] = $_REQUEST['to'] ){
+	if(! captcha_check( captcha , $_REQUEST['captcha'] ) ) {
+			
+		echo "<div class='errors'><h2 style=\"font-size: 18px;\">کد امنیتی اشتباه است</h2></div>";
+				
+	} else if(! $_REQUEST['to'] = $_REQUEST['to'] ){
 		e(__FUNCTION__,__LINE__);
 	
 	} else if(! $_REQUEST['to'] = setting($_REQUEST['to']) ){
