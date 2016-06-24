6d1166f191cd827c8d5aa0509a83ab71db52b363
Jalal Hoseini <jalal7h@gmail.com>
2016-06-24 22:02:02 +0430

    dbsymc gitignore

--- en/components/listmaker-2/listmaker_list.js
+++ en/components/listmaker-2/listmaker_list.js
@@ -6,11 +6,11 @@ $(document).ready(function(){
 	// mouse over effect on TR
 	$('.tools-td').on("mouseenter", function(){
 		lml_intoolstd = "in";
-		console.log(lml_intoolstd);
+		// cl(lml_intoolstd);
 	});
 	$('.tools-td').on("mouseleave", function(){
 		lml_intoolstd = "out";
-		console.log(lml_intoolstd);
+		// cl(lml_intoolstd);
 	});
 })
 
