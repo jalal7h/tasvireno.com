bf8f49e035040d9f6b5223a88ff18b5daf834503
Jalal Hoseini <jalal7h@gmail.com>
2017-01-09 10:04:06 +0330

    fix template engine bug.

--- components/contact-3-spi/contact_vw_send.php
+++ components/contact-3-spi/contact_vw_send.php
@@ -2,8 +2,7 @@
 
 function contact_vw_send(){
 
-	if(! captcha_check( captcha , $_REQUEST['captcha'] ) ) {
-			
+	if(! captcha_check( captcha , $_REQUEST['captcha'] ) ) {	
 		echo "<div class='errors'><h2 style=\"font-size: 18px;\">کد امنیتی اشتباه است</h2></div>";
 				
 	} else if(! $_REQUEST['to'] = $_REQUEST['to'] ){
