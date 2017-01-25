66d1f54943c058c3937bf971ae69dfaa8eecd8e0
Jalal Hoseini <jalal7h@gmail.com>
2017-01-26 00:36:48 +0330

    .

--- en/components/recaptcha-1/recaptcha_check.php
+++ en/components/recaptcha-1/recaptcha_check.php
@@ -7,13 +7,13 @@
 function recaptcha_check(){
 	
 	if(! $response = trim($_REQUEST['g-recaptcha-response']) ){
-		e(__FUNCTION__, __LINE__);
+		// e(__FUNCTION__, __LINE__);
 
 	} else if(! $res = curl( 'https://www.google.com/recaptcha/api/siteverify', [
 		'secret'	=> recaptcha_secred_key , 
 		'response'	=> $response
 	] ) ){
-		e(__FUNCTION__, __LINE__);
+		// e(__FUNCTION__, __LINE__);
 
 	} else {
 
