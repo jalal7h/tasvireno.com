07e76c77525a6d758d89c6064242a86125476998
Jalal Hoseini <jalal7h@gmail.com>
2017-01-26 00:01:18 +0330

    recaptcha

--- components/contact-3-spi/contact_vw_send.php
+++ components/contact-3-spi/contact_vw_send.php
@@ -4,10 +4,12 @@ function contact_vw_send(){
 
 	$ip = $_SERVER['REMOTE_ADDR'];
 
-	if(! token_check() ){
-		e(__FUNCTION__,__FILE__);
+	// if(! token_check() ){
+		// e(__FUNCTION__,__FILE__);
 
-	} else if(! captcha_check( 'contact' , $_REQUEST['captcha'] ) ) {	
+	// } else if(! captcha_check( 'contact' , $_REQUEST['captcha'] ) ) {
+	// } else 
+	if(! recaptcha_check() ){
 		echo "<div class='errors'><h2 style=\"font-size: 18px;\">کد امنیتی اشتباه است</h2></div>";
 				
 	} else if(! $_REQUEST['to'] = $_REQUEST['to'] ){
